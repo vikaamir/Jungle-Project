72a8ce8af2018778e91453a5332ce3e2934a2ef0
Victoria Amirov <vika.amir20@gmail.com>
2023-10-29 12:27:24 -0700

    update the dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#show'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
