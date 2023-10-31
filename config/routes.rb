6ad7a7dcae7be0183e7aff73c0c0edc69ebb1f36
Victoria Amirov <vika.amir20@gmail.com>
2023-10-30 17:01:06 -0700

    added Feature Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
     get 'dashboard', to: 'dashboard#show'
   end
 
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
